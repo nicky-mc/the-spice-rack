bb408a4c0fa286bcd05ae8e52aa8ee5851cff5e7
nicky-mc <cinimod@gmail.com>
2024-10-24 15:45:04 +0100

    added dbconnection and supabaseclient and env files

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// Define protected routes that require authentication
-const isProtectedRoute = createRouteMatcher([
+// Define public routes that do not require authentication
+const isPublicRoute = createRouteMatcher([
+  "/about",
   "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   "/(api|trpc)(.*)",
 ]);
 
 // Use clerkMiddleware to protect the routes
 export default clerkMiddleware((req) => {
-  if (isProtectedRoute(req) && !req.nextUrl.pathname.startsWith("/about")) {
+  if (isPublicRoute(req)) {
     return NextResponse.next();
   }
-  return NextResponse.next();
+  return NextResponse.redirect("/sign-in");
 });
 
 export const config = {
