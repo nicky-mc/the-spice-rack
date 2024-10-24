6417af90eeb25509085f7efcdf1e347e67c5e008
nicky-mc <cinimod@gmail.com>
2024-10-24 15:51:46 +0100

    added dbconnection and supabaseclient and env files

--- src/middleware.js
+++ src/middleware.js
@@ -17,35 +17,6 @@ export default clerkMiddleware((req) => {
 });
 
 export const config = {
-  // The matcher will find matches for public and private routes
-  // If the match is for a public route, middleware does not trigger  import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-  import { NextResponse } from "next/server";
-  
-  // Define public routes that do not require authentication
-  const isPublicRoute = createRouteMatcher([
-    "/about",
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ]);
-  
-  // Use clerkMiddleware to protect the routes
-  export default clerkMiddleware((req) => {
-    if (isPublicRoute(req)) {
-      return NextResponse.next();
-    }
-    return NextResponse.redirect("/sign-in");
-  });
-  
-  export const config = {
-    matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-      "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-      // Always run for API routes
-      "/(api|trpc)(.*)",
-    ],
-  };
-  // If the match is for a private route, middleware will trigger with authentication
-  // The matcher uses regex to find matches
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
