d0fe48b2e57d57539fada5744904d58b09070a02
nicky-mc <cinimod@gmail.com>
2024-10-24 15:41:16 +0100

    added dbconnection and supabaseclient and env files

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
+// Define protected routes that require authentication
+const isProtectedRoute = createRouteMatcher([
+  "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  "/(api|trpc)(.*)",
+]);
 
 // Use clerkMiddleware to protect the routes
 export default clerkMiddleware((req) => {
-  console.log("Request Object:", req);
-
-  // Log the request object
+  if (isProtectedRoute(req) && !req.nextUrl.pathname.startsWith("/about")) {
+    return NextResponse.next();
+  }
   return NextResponse.next();
 });
 
